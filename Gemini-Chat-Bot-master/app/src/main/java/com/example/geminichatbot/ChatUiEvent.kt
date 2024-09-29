36120b5eaf452e2acf7ff2bb67b1fea8a8e4833b
unknown <kushalpatil006>
2024-09-29 19:30:48 +0530

    first commit

---
+++ Gemini-Chat-Bot-master/app/src/main/java/com/example/geminichatbot/ChatUiEvent.kt
@@ -0,0 +1,13 @@
+package com.example.geminichatbot
+
+
+import android.graphics.Bitmap
+
+
+sealed class ChatUiEvent {
+    data class UpdatePrompt(val newPrompt: String) : ChatUiEvent()
+    data class SendPrompt(
+        val prompt: String,
+        val bitmap: Bitmap?
+    ) : ChatUiEvent()
+}
